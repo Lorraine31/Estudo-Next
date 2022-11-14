22320473992e0911264e57b1ca8d810f7f6c520a
Lorraine Letiele Pires <lorraine.letiele@vitat.com.br>
2022-11-14 10:55:34 -0300

    continuação dos estudos sobre next.js

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+export default function Home({ allPostsData }) {
+  return (
+    
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Seja bem-vindo! Me chamo Lorraine e sou uma desenvolvedora que atualmente está aprendendo Next.js</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
+
